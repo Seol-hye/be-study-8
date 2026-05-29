0088e458b9f0222a0f7077586fbef7c220bc7ec4
Seol-hye <hs1507lll@gmail.com>
2026-05-29 14:47:20 +0900

    클래스 다형성 개념 예시, 강제타입변환, instanceof

---
+++ src/be_study/inherit/inherit05/v3/DustCleaner.java
@@ -0,0 +1,14 @@
+package be_study.inherit.inherit05.v3;
+
+public class DustCleaner extends Cleaner{
+	
+	public void doClean() {
+		System.out.println("먼지털기~");
+	}
+
+	public void arrangeItem() {
+		// TODO Auto-generated method stub
+		
+	}
+
+}
