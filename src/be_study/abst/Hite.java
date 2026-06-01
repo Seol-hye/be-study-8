aff5cf03c34171784fef04f58b50a4dfd01b2db1
Seol-hye <hs1507lll@gmail.com>
2026-06-01 14:22:53 +0900

    추상 클래스

---
+++ src/be_study/abst/Hite.java
@@ -0,0 +1,15 @@
+package be_study.abst;
+
+public class Hite extends Beverage {
+	
+	double alcol; //몇도짜리인지
+	
+	public void checkAlcol() {
+		System.out.println("Hite 알콜 도수 확인");
+	}
+	
+	public void checkSafety() {
+		System.out.println("알콜 도수 문제 확인");
+	}
+
+}
