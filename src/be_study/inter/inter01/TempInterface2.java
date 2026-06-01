63e0242b10d2d846df10c245bda74dd20de3b9a6
Seol-hye <hs1507lll@gmail.com>
2026-06-01 15:55:39 +0900

    인터페이스 개념

---
+++ src/be_study/inter/inter01/TempInterface2.java
@@ -0,0 +1,10 @@
+package be_study.inter.inter01;
+
+public interface TempInterface2 {
+	
+	public abstract void method2(); //추상메소드 선언
+	
+	public void method3();
+	// abstract 가 다 붙어있음
+	
+}
---
+++ src/be_study/inter/inter02/A.java
@@ -0,0 +1,6 @@
+package be_study.inter.inter02;
+
+public interface A {
+	public void a();
+
+}
